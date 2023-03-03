20075e5942f4b34f58989e6384cc15c1737fcbbe
rafaelst2000 <rafaelst2000@gmail.com>
2023-03-03 16:52:41 -0300

    feat(Layout): add antd global provider

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { ConfigProvider } from 'antd'
 
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 
 import { defaultTheme } from './styles/themes/default'
 
+import ptBR from 'antd/lib/locale/pt_BR'
+
 export function LayoutApp() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <GlobalStyle />
+      <ConfigProvider locale={ptBR}>
+        <BrowserRouter>
+          <GlobalStyle /> 
 
-        <Router />
-      </BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ConfigProvider>
     </ThemeProvider>
   )
 }
